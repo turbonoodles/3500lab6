ce1f9b8ad473a6fa1f0ed8eefda04364c6fac800
Craig McMahon <cmcmahon@sgl.com>
2021-03-03 00:37:33 -0500

    final patches to bcd decoder

--- lab8_1_2/src/hdl/bcdto7segment_dataflow.v
+++ lab8_1_2/src/hdl/bcdto7segment_dataflow.v
@@ -36,11 +36,11 @@ assign d = bcd_in[0];
 // segment equations
 // inverted sum-of-products because I'm a dummy & didn't notice active-low
 assign seg[0] = ~( a | c | ( b & d ) | ( ~b & ~d ) ); // segment a
-assign seg[1] = ~( a | ~b | ( c & d ) | ( ~c & ~d )); // segment b
-assign seg[2] = ~( a | b | ~c | d ); // segment c
+assign seg[1] = ~( ~b | ( c & d ) | ( ~c & ~d )); // segment b
+assign seg[2] = ~( b | ~c | d ); // segment c
 assign seg[3] = ~( a | (c & ~d) | (~b & (c | ~d)) | (b & ~c & d) ); // segment d
 assign seg[4] = ~( ( c & ~d ) | ( ~b & ~d )); // segment e 
-assign seg[5] = ~( (~c & ~d) | (b & ~c) | (b & ~d) ); // segment f
-assign seg[6] = ~( a | (b & ~c) | (~b & ~c) | (c & ~d)); // segment g
+assign seg[5] = ~( a | (~c & ~d) | (b & ~c) | (b & ~d) ); // segment f
+assign seg[6] = ~( a | (b & ~c) | (~b & c) | (c & ~d)); // segment g
 
 endmodule
